c4cacf77d8af4089da5dfc1735e5de38db3d741a
JonaSeguReymundo <00043823@uca.edu.sv>
2024-09-04 12:03:16 -0600

    Update

--- main.js
+++ main.js
@@ -1,6 +1,21 @@
 const person = {
     events: ["drunk", "drive", "workout"],
     accident: true
-}
+};
+
 console.log(person.events[0]);
 console.log(person.accident);
+
+const journal = [{
+        events: ["drunk", "drive", "workout"],
+        accident: true
+    },
+    {
+        events: ["cook", "run", "call"],
+        accident: false
+    },
+    {
+        events: ["talk", "sleep", "study"],
+        accident: false
+    }
+];
